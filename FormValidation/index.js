c9afcb199de62bc5893d1f953b90b9ed7b4f9b31
sakibalamin <sakibalamin162@gmail.com>
2020-08-17 01:41:30 -0700

    index & register html updated

--- FormValidation/index.js
+++ FormValidation/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
 const PORT = 3000;
 
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
+
+const urlencodeParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/', (req, res) => {
     res.render('index.html');
@@ -12,6 +16,10 @@ app.get('/', (req, res) => {
 
 app.get('/register', (req, res) => {
     res.render('register.html');
+});
+
+app.post('/register', urlencodeParser, (req, res) => {
+    res.json(req.body);
 });
 
 app.listen(PORT, () => console.log(`app running on port ${PORT}`));
