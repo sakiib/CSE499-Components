da8c6b0904c57b689e04e6a21351bba6c9851c89
sakibalamin <sakibalamin162@gmail.com>
2020-08-18 18:49:08 -0700

    hashed password with bcryptjs

--- API-Auth-JWT/routes/auth.js
+++ API-Auth-JWT/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const { registerValidation, loginValidation } = require('./validation');
 
 router.post('/register', async (req, res) => {
@@ -12,11 +13,15 @@ router.post('/register', async (req, res) => {
     const emailExist = await User.findOne({email: req.body.email});
     if (emailExist) return res.status(400).send('Email already exists');
 
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    
     // create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try {
         const savedUser = await user.save();
