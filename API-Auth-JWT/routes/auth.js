3ea95a3edfa4151cd2f1035260705981c0366d9f
sakibalamin <sakibalamin162@gmail.com>
2020-08-18 18:43:49 -0700

    email exist in db verified

--- API-Auth-JWT/routes/auth.js
+++ API-Auth-JWT/routes/auth.js
@@ -4,10 +4,15 @@ const User = require('../model/User');
 const { registerValidation, loginValidation } = require('./validation');
 
 router.post('/register', async (req, res) => {
-    
-    const {error} = registerValidation(req.body);
+    // validate data before creating a user
+    const { error } = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
+    // checking if user email is already registered in databasee
+    const emailExist = await User.findOne({email: req.body.email});
+    if (emailExist) return res.status(400).send('Email already exists');
+
+    // create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
